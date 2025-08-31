e46a47de91f02ca9ea0d99c0bc70080bd30f09dd
AwesomeDadNO <ingebrigtsen.arnfinn@gmail.com>
2025-08-31 22:35:03 +0200

    xteve addon

---
+++ a-xteve/README.md
@@ -0,0 +1,100 @@
+# Lazystream/IPTV Media Server Bridge
+
+This Docker provides a simple solution to get live NHL and MLB games into your Emby, Plex, or Channels live TV setup. When combined with a comprehensive IPTV package this setup can get you rolling with live TV and high quality sports. If you would only like to use this for the available sports, simply disable guide2go (or any other function you would like). Please see the Wiki for some user-created scripts to help get things setup.
+
+- [Run](#run)
+- [Setup](#setup)
+  - [Env file](#env-file)
+  - [Cron schedule](#cron-schedule)
+  - [Sample volume mapping](#sample-volume-mapping)
+  - [xTeVe](#xteve)
+  - [guide2go](#guide2go)
+  - [Testing cronjob function](#testing-cronjob-function)
+  - [Notes on Channels DVR](#notes-on-channels-dvr)
+- [Credits](#credits)
+  - [guide2go](#guide2go-1)
+  - [Lazystream](#lazystream)
+  - [xTeVe](#xteve-1)
+
+# Run
+
+```
+docker run -d \
+	--name=xteve_lazystream \
+	-p 34400:34400 \
+	-e TZ="America/Los_Angeles" \
+	--env-file=.env \
+	--log-opt max-size=10m \
+	--log-opt max-file=3 \
+	-v /mnt/user/appdata/xteve/.xteve:/xteve:rw \
+	-v /mnt/user/appdata/xteve/config/:/config:rw \
+	-v /mnt/user/appdata/xteve/guide2go/:/guide2go:rw \
+	-v /mnt/user/appdata/xteve/playlists/:/playlists:rw \
+	-v /tmp/xteve/:/tmp/xteve:rw \
+	taylorbourne/xteve_lazystream
+```
+
+or see [docker-compose.yml](docker-compose.yml) for an example docker-compose setup.
+
+# Setup
+
+## Env file
+
+Defaults & behavior can be changed through environment variables. `sample.env` should be renamed to `.env` and supplied through the `--env-file` docker run option. The `.env` file can also be picked up if using this in a `docker compose` setup.
+
+## Cron schedule
+
+By default, the cron job is scheduled to run every hour. A custom cron schedule can be specified by renaming the `sample_cron.txt` file in the `/config` volume to `cron.txt` and editing the schedule. Make sure to restart your container to take effect.
+
+## Sample volume mapping
+
+| Container Path | Host Path                         |
+| -------------- | --------------------------------- |
+| /xteve         | /mnt/user/appdata/xteve/.xteve    |
+| /config        | /mnt/user/appdata/xteve/config    |
+| /guide2go      | /mnt/user/appdata/xteve/guide2go  |
+| /playlists     | /mnt/user/appdata/xteve/playlists |
+| /tmp/xteve     | /tmp/xteve                        |
+
+## xTeVe
+
+By default, xTeVe is setup with all 200 channels mapped for NHL and MLB games. This can be setup directly with Plex / Emby to work out of the box without any changes. If needed, changes can be made to xTeVe by going to `127.0.0.1:34400/web/`.
+
+## guide2go
+
+If you have an existing guide2go setup you may copy the `.yaml` files into the path mounted at `/guide2go`. Otherwise run the following command and follow the on-screen steps  
+`docker exec -it dockername guide2go -configure /guide2go/your_epg_name.yaml`
+
+## Testing cronjob function
+
+Simply run the cronjob file inside the Docker container  
+`docker exec -it dockername ./cronjob.sh`
+
+## Notes On Channels DVR
+
+- You have to select MPEG-TS as the stream format
+- The stream only works in a browser - not on Android TV or Android
+- Adding an m3u playlist to Channels:
+  https://getchannels.com/docs/channels-dvr-server/how-to/custom-channels/#adding-your-custom-channels
+
+# Credits
+
+[Lazystream](https://github.com/tarkah/lazystream) – [@tarkah](https://github.com/tarkah/)  
+[xTeVe](https://github.com/xteve-project/xTeVe) and [guide2go](https://github.com/mar-mei/guide2go) – [@marmei](https://github.com/mar-mei)  
+Original author of the [xTeVe/guide2go Docker](https://github.com/alturismo/xteve_guide2go) – [@alturismo](https://github.com/alturismo)
+
+## guide2go
+
+XMLTV EPG grabber for Schedules Direct, thanks to @marmei  
+GitHub: https://github.com/mar-mei/guide2go  
+Schedules Direct: http://www.schedulesdirect.org/
+
+## Lazystream
+
+GitHub: https://github.com/tarkah/lazystream
+
+## xTeVe
+
+IPTV and EPG proxy server for Plex, Emby, etc – thanks to @marmei  
+Website: http://xteve.de  
+Discord: https://discordapp.com/channels/465222357754314767/465222357754314773
---
+++ a-xteve/root/cronjob.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/with-contenv bash
+
+function prepend() {
+	time=`date +"%Y/%m/%d %H:%M:%S"`
+	while read line;
+	do echo "${time} ${1}${line}";
+	done;
+}
+
+exec 1> >(prepend "[cronjob.sh] ")
+
+echo "Running scripts..."
+
+### Generate playlist and XML data from Lazystream
+if [ "$use_lazystream" = "yes" ]; then
+
+	args=()
+
+	if [ "$trim_xmltv" = "yes" ]; then args+=("--trim"); fi
+	if [ ! -z ${quality} ]; then args+=("--quality" "$quality"); fi
+	if [ "$cdn" = "l3c" ]; then args+=("--cdn" "l3c"); fi
+	if [ ! -z "$hostOverride" ]; then args+=("--host" "$hostOverride"); fi
+
+	if [ "$include_nhl" = "yes" ]; then
+		echo "Running Lazystream (NHL $quality via $cdn)..."
+		mkdir -p /playlists/lazystream
+
+		nhl_args=()
+
+		nhl_args+=("--channel-prefix")
+		nhl_args+=("Lazystream: NHL")
+		nhl_args+=("--start-channel")
+		if [ -z "$nhl_start_channel" ]; then
+			nhl_args+=("1000")
+		else
+			nhl_args+=("$nhl_start_channel")
+		fi
+		nhl_args+=("/playlists/lazystream/lazystream-nhl")
+
+		if [ "$nhl_exclude_home" = "yes" ]; 	 then nhl_args+=("--exclude-feeds" "HOME"); fi
+		if [ "$nhl_exclude_away" = "yes" ]; 	 then nhl_args+=("--exclude-feeds" "AWAY"); fi
+		if [ "$nhl_exclude_national" = "yes" ];  then nhl_args+=("--exclude-feeds" "NATIONAL"); fi
+		if [ "$nhl_exclude_french" = "yes" ]; 	 then nhl_args+=("--exclude-feeds" "FRENCH"); fi
+		if [ "$nhl_exclude_composite" = "yes" ]; then nhl_args+=("--exclude-feeds" "COMPOSITE"); fi
+
+		set -x
+		lazystream generate xmltv "${args[@]}" "${nhl_args[@]}"
+		set +x
+	fi
+	if [ "$include_mlb" = "yes" ]; then
+		echo "Running Lazystream (MLB $quality via $cdn)..."
+		mkdir -p /playlists/lazystream
+
+		mlb_args=()
+
+		mlb_args+=("--sport")
+		mlb_args+=("mlb")
+		mlb_args+=("--channel-prefix")
+		mlb_args+=("Lazystream: MLB")
+		mlb_args+=("--start-channel")
+		if [ -z "$mlb_start_channel" ]; then
+			mlb_args+=("2000")
+		else
+			mlb_args+=("$mlb_start_channel")
+		fi
+		mlb_args+=("/playlists/lazystream/lazystream-mlb")
+
+		if [ "$mlb_exclude_home" = "yes" ]; 	 then mlb_args+=("--exclude-feeds" "HOME"); fi
+		if [ "$mlb_exclude_away" = "yes" ]; 	 then mlb_args+=("--exclude-feeds" "AWAY"); fi
+		if [ "$mlb_exclude_national" = "yes" ];  then mlb_args+=("--exclude-feeds" "NATIONAL"); fi
+		if [ "$mlb_exclude_composite" = "yes" ]; then mlb_args+=("--exclude-feeds" "COMPOSITE"); fi
+
+		set -x
+		lazystream generate xmltv "${args[@]}" "${mlb_args[@]}"
+		set +x
+	fi
+fi
+
+# run guide2go in a loop
+if [ "$use_guide2go" = "yes" ]; then
+	echo "Running guide2Go..."
+	for yamls in $YamlList
+		do
+		yamlfile="${yamls%.*}"
+		filecache='  "file.cache": "/guide2go/cache_'$yamls'",'
+		fileoutput='  "file.output": "/guide2go/'$yamlfile'.xml",'
+		sed -i "/file.cache/c $filecache" /guide2go/$yamls
+		sed -i "/file.output/c $fileoutput" /guide2go/$yamls
+		guide2go -config /guide2go/$yamls
+	done
+fi
+
+sleep 1
+
+# update xteve via API
+if [ "$use_xTeveAPI" = "yes" ]; then
+	echo "Updating xTeVe..."
+	curl -s -X POST -d '{"cmd":"update.m3u"}' http://127.0.0.1:$XTEVE_PORT/api/
+	# sleep 1
+	curl -s -X POST -d '{"cmd":"update.xmltv"}' http://127.0.0.1:$XTEVE_PORT/api/
+	sleep 1
+	curl -s -X POST -d '{"cmd":"update.xepg"}' http://127.0.0.1:$XTEVE_PORT/api/
+	sleep 30
+fi
+
+# update Emby via API
+if [ "$use_embyAPI" = "yes" ]; then
+	echo "Updating Emby..."
+	if [ -z "$embyIP" ]; then
+		echo "no Emby credentials provided"
+	else
+		curl -s -X POST "http://$embyIP:$embyPORT/emby/ScheduledTasks/Running/$embyID?api_key=$embyApiKey" -H "accept: */*" -d ""
+		sleep 1
+	fi
+fi
+
+# update Plex via API
+if [ "$use_plexAPI" = "yes" ]; then
+	
+	# get protocol
+	proto="$(echo $plexUpdateURL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+	# remove the protocol
+	url="$(echo ${plexUpdateURL/$proto/})"
+	# extract the host
+	plexHostPort="$(echo ${url/} | cut -d/ -f1)"
+	
+	echo "Updating Plex..."
+	if [ -z "$plexUpdateURL" ]; then
+		echo "no Plex credentials provided"
+	else
+		curl --location --request POST "$plexUpdateURL" -H "authority: $plexHostPort" -H "content-length: 0" -H "pragma: no-cache" -H "cache-control: no-cache" -H "sec-ch-ua: 'Google Chrome';v='95', 'Chromium';v='95', ';Not A Brand';v='99'" -H "accept: text/plain, */*; q=0.01" -H "x-requested-with: XMLHttpRequest" -H "accept-language: en" -H "sec-ch-ua-mobile: ?0" -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36" -H "sec-ch-ua-platform: 'macOS'" -H "origin: http://$plexHostPort" -H "sec-fetch-site: same-origin" -H "sec-fetch-mode: cors" -H "sec-fetch-dest: empty" -H "referer: http://$plexHostPort/web/index.html"
+		sleep 1
+	fi
+fi
+
+# update Channels via API
+if [ "$use_channelsAPI" = "yes" ]; then
+	echo "Updating Channels..."
+	if [ -z "$channelsUpdateM3uURL" ]; then
+		echo "no Channels M3U URL provided"
+	else
+		curl -s -X POST "$channelsUpdateM3uURL"
+		sleep 1
+	fi
+	if [ -z "$channelsUpdateXmltvURL" ]; then
+		echo "no Channels XMLTV URL provided"
+	else
+		curl -s -X PUT "$channelsUpdateXmltvURL"
+		sleep 1
+	fi
+fi
+
+exit
